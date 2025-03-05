c215d44c6c7ee03ca58dd65fca3e92f6f5fc2fe8
ArevaloDev <arevalodaniel999@gmail.com>
2025-03-04 22:13:00 -0600

    agregando variables y funciones para convertir moneda

--- convertidor/src/app/components/convertidor/convertidor.component.ts
+++ convertidor/src/app/components/convertidor/convertidor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConvertidorMonedasService } from '../../services/convertidor-monedas.service';
 
 @Component({
   selector: 'app-convertidor',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ConvertidorComponent {
 
+  public cantidad:number = 0;
+  public monedaOrigen = 'USD';
+  public monedaDestino = 'EUR';
+  public total:number = 0;
+  public monedas:string[] = ['USD', 'EUR', 'LIBRA', 'MXN', 'GTQ'];
+
+  constructor(private monedaService:ConvertidorMonedasService){}
+
+  convertir = () => {
+    this.total = this.monedaService.convertirMoneda(this.cantidad, this.monedaOrigen, this.monedaDestino);
+  }
 }
